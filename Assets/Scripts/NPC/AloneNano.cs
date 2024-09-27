e2b4aa2218e86c4dd049bf10eeb88059b5b52f1e
Yuzhe Xie <xyz_yfy@163.com>
2024-09-27 14:53:15 -0400

    alonenanofix

--- Assets/Scripts/NPC/AloneNano.cs
+++ Assets/Scripts/NPC/AloneNano.cs
@@ -7,9 +7,9 @@ public class AloneNano : Nanobot
 {
     public UnityEvent afterSparing;
     public ConditionObject killedNanocell;
+
     void OnTriggerExit2D(Collider2D collider) {
-        if(collider.gameObject.tag == "plaeyr") {
-            Debug.Log("yes");
+        if(collider.gameObject.tag == "player") {
             if(killedNanocell.value == false) {
                 afterSparing.Invoke();
             }
